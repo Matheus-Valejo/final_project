ce6646137a7d216d5e4b28543044e62d34c91e9a
Matheus-Valejo <87394495+Matheus-Valejo@users.noreply.github.com>
2024-04-25 15:27:53 -0400

    credential check

--- secureAuthentication/client.py
+++ secureAuthentication/client.py
@@ -1,5 +1,6 @@
 import socket
 
+
 def client_start():
 
     HOST = "127.0.0.1"
@@ -13,11 +14,28 @@ def client_start():
         print("socket creation failed with error %s" % (err))
 
     while True:
-        data = input("Enter message  -  ('Close' to close connection):  ")
-        client.send(data.encode("utf-8")[:1024])
-
-        if (data.lower() == "close"):
-            break
+        #Three states
+        #1. Login state/2 successful login(user can use the server)/3 login failure (user is kicked out from the server)
+        prompt = client.recv(1024).decode("utf-8")
+        if(prompt == "Insert Username:"):
+            print(prompt)             
+            data = input()
+            client.send(data.encode("utf-8"))
+            prompt = client.recv(1024).decode("utf-8")
+            print(prompt)  
+            data = input()
+            client.send(data.encode("utf-8"))
+            prompt = client.recv(1024).decode("utf-8")
+        if(prompt == "Login successful"):    
+            while True:
+                data = input("Enter message  -  ('Close' to close connection):  ")
+                client.send(data.encode("utf-8"))
+                if (data.lower() == "close"):
+                    client.close()
+                    print("Connection to server closed")
+        if(prompt == "Login failure"):  
+            client.close()
+        
         else:
             response = client.recv(1024)
             response = response.decode("utf-8")
@@ -25,8 +43,7 @@ def client_start():
 
 
     # close client socket (connection to the server)
-    client.close()
-    print("Connection to server closed")
+    
 
     return 0
 
--- secureAuthentication/server.py
+++ secureAuthentication/server.py
@@ -1,6 +1,7 @@
 import socket
 
 #define function to initialize socket connection
+
 def server_start():
     HOST = "127.0.0.1"
     port = 54321
@@ -11,31 +12,58 @@ def server_start():
         print("Socket successfully created")
     except socket.error as err:
         print("socket creation failed with error %s" % (err))
+        return
 
     server.listen(5)
     print("Server accepting connections\n")
-
+    
     client_socket, addr = server.accept()
     print(f"Accepted Connection from client address {addr}")
-
+    
     while True:
-        response = client_socket.recv(1024)
-        response = response.decode("utf-8")
+    
+        try:
+            client_socket.send("Insert Username:".encode("utf-8"))
+            username = client_socket.recv(1024).decode("utf-8")
+            client_socket.send("Insert Password:".encode("utf-8"))
+            password = client_socket.recv(1024).decode("utf-8")
+            if(check_credentials(username,password)):
+                print("Login successful")
+                client_socket.send("Login successful".encode("utf-8"))
+                handle_client(client_socket)
+            else:
+                client_socket.send("Login failure")
+                
+                
+                print(f"Client connection at address {addr} closed")
+        except Exception as e:
+                print(f"Error handling connection from {addr}: {e}")
+                client_socket.close()
+    
+#def function that checks for the user credential (no hash yet)
+def check_credentials(username, password):
+    with open("credentials.txt", "r") as file:
+        for line in file:
+            stored_username, stored_password = line.strip().split(',')
+            if stored_username == username and stored_password == password:
+                return True 
+    return False
 
-        if (response.lower=="close"):
-            break
-        else:
-            print(response)
-            data = "Message Received".encode("utf-8")
-            client_socket.send(data)
-
-
-    client_socket.close()
-    print(f"Client connection at address {addr} closed")
-    server.close()
-    print("Server Closed")
-    return 0
-
+def handle_client(client_socket, addr):
+    try:
+        while True:
+            response = client_socket.recv(1024)
+            response = response.decode("utf-8")
+            if (response.lower=="close"):
+                break
+            else:
+                print(response)
+                data = "Message Received".encode("utf-8")
+                client_socket.send(data)
+    finally:
+        client_socket.close()
+        print(f"Client connection at address {addr} closed")
+        
 
 if __name__ == '__main__':
     server_start()
